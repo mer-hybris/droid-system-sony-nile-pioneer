19f6a5c43f4e45cad25c03a0343c7f166b5138bf
Franz-Josef Haider <franz.haider@jolla.com>
2018-07-05 12:03:42 +0200

    [h4113] Initial content. JB#42314

---
+++ copy_system.sh
@@ -0,0 +1,34 @@
+
+#
+# Before running this script please extract raw system image
+# and mount loop image to some mount point. Give that mount point
+# parameter for this script.
+#
+# 1. "simg2img system.img system.raw" (Run inside Mer SDK target)
+# 2. "mkdir ~/system"
+# 3. "sudo mount -t ext4 -o loop system.raw ~/system"
+# 4. "./copy_system.sh ~/system/system"
+#
+
+if [ -z "$1" ]; then
+  echo "No argument supplied, try $0 ~/path/to/system"
+  exit
+fi
+
+SYSTEM_SPARSE="sparse/system"
+SYSTEM_MOUNT=$1
+
+# Add read permission for some binaries under system mount
+source droid-system-device/helpers/fixup-permissions.sh
+
+# Remove current sparse and create it again
+rm -rf $SYSTEM_SPARSE
+mkdir -p $SYSTEM_SPARSE
+
+# Copy content
+echo "Copy $SYSTEM_MOUNT/* to $SYSTEM_SPARSE"
+cp -r $SYSTEM_MOUNT/* $SYSTEM_SPARSE
+
+# Remove generic unused directories and files
+source droid-system-device/helpers/remove-unused.sh
+
---
+++ rpm/droid-system-h4113.spec
@@ -0,0 +1,6 @@
+%define device h4113
+%define rpm_device h4113
+
+%define dsd_path ./
+
+%include droid-system-device/droid-system.inc
